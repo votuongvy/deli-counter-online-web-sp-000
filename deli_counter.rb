ecb73e66f01a21cd951511365b198fb26011ea6d
vy vo <tuonggvy.vo@gmail.com>
2019-11-03 03:31:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+def line(katz_deli)
+  if katz_deli.size == 0
+    "The line is currently empty."
+  else
+    "The line is currently: kat"
+  end
+end
+
+def take_a_number(katz_deli, position)
+  position = katz_deli.size + 1
+  puts "Welcome, katz_deli[-1]. You are number #{position} in line."
+end
