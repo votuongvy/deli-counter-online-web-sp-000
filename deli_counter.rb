69102f2ba65b78fd4a9030c367de1641d4413993
vy vo <tuonggvy.vo@gmail.com>
2019-11-03 03:48:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,6 +5,9 @@ def line(x)
   line_array = []
   if x.length == 0
     "The line is currently empty."
+  else
+     x.each.with_index(1) do |name, index|
+      line_array.push("#{index}. #{name}")
   end
 end
 
